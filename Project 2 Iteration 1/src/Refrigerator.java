326b40b0a4a7f879565d1371016fb574f4dc7d79
aliciastruble <aliciastruble@gmail.com>
2015-03-31 16:30:18 -0500

    Added temperature variables and a method to set them from input data file.
    Signed-off-by: aliciastruble <aliciastruble@gmail.com>

--- Project 2 Iteration 1/src/Refrigerator.java
+++ Project 2 Iteration 1/src/Refrigerator.java
@@ -5,10 +5,25 @@ public class Refrigerator {
 	private static Freezer freezer;
 	private static final boolean DOOR_OPENED = true;
 	private static final boolean DOOR_CLOSED = false;
+	int fridgeLow; 
+	int fridgeHigh; 
+	int freezerLow;
+	int freezerHigh; 
+	int roomLow;
+	int roomHigh; 
+	int fridgeUp1DoorClosed;
+	int fridgeUp1DoorOpen; 
+	int freezerUp1DoorClosed; 
+	int freezerUp1DoorOpen;
+	int tempDiffToStartCoolFridge;
+	int tempDifftoStartCoolFreezer;
+	int minutesToCoolFridge1;
+	int minutesToCoolFreezer1;
+	
 
 	// reference back to the gui so this class can set the status labels
 	private GUI gui;
-	
+
 	private Refrigerator(){
 		fridge = Fridge.instance();
 		freezer = Freezer.instance();
@@ -43,22 +58,46 @@ public class Refrigerator {
 	
 	public void openFridgeDoor(){
 		fridge.setDoor(DOOR_OPENED);
+		gui.setFridgeLightLbl(gui.FRIDGE_LIGHT_ON);
 	}
 	
 	public void closeFridgeDoor(){
 		fridge.setDoor(DOOR_CLOSED);
+		gui.setFridgeLightLbl(gui.FRIDGE_LIGHT_OFF);
 	}
 	
 	public void openFreezerDoor(){
 		freezer.setDoor(DOOR_OPENED);
+		gui.setFreezerLightLbl(gui.FREEZER_LIGHT_ON);
 		
 	}
 	
 	public void closeFreezerDoor(){
 		freezer.setDoor(DOOR_CLOSED);
+		gui.setFreezerLightLbl(gui.FREEZER_LIGHT_OFF);
 	}
 	
 	public void clockTicked(){
+		
+	}
+	
+	public void setData(int[] data){
+		if(data.length == 14){
+		 fridgeLow = data[0]; 
+		 fridgeHigh = data[1]; 
+		 freezerLow = data[2];
+		 freezerHigh = data[3]; 
+		 roomLow = data[4];
+		 roomHigh = data[5]; 
+		 fridgeUp1DoorClosed = data[6];
+		 fridgeUp1DoorOpen = data[7]; 
+		 freezerUp1DoorClosed = data[8]; 
+		 freezerUp1DoorOpen = data[9];
+		 tempDiffToStartCoolFridge = data[10];
+		 tempDifftoStartCoolFreezer = data[11];
+		 minutesToCoolFridge1 = data[12];
+		 minutesToCoolFreezer1 = data[13];
+		}
 		
 	}
 }
